db2a54c5803e0bf895cc9fb79d9ffb889bfecd05
kjetilstockholm <kjetilstockholm@users.noreply.github.com>
2026-02-24 21:46:09 +0000

    deploy: a4641d7f9821e764ab19f8fa78036e6670b36980

--- assets/slidev/default-DmJPQzoe.js
+++
@@ -1,1 +0,0 @@
-import{o,e as t,i as s}from"../modules/vue-DVtSp9EU.js";import{_ as r}from"../index-OrHMPlOo.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
